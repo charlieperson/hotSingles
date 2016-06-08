f8cba174e7620ce7644e58a12aaf5f79fb909706
charlieperson <charlie.person@hyperisland.se>
2016-06-09 01:09:35 +0200

    Solved- could use refactoring

--- hotSingles.js
+++ hotSingles.js
@@ -1,4 +1,18 @@
 function hotSingles(arr1, arr2) {
-  var array_one = arr1;
-  var array_two = arr2;
+  var both = arr1.concat(arr2);
+  var duplicates = [];
+
+  for(i=0; i<arr1.length; i++) {
+    for(x=0; x<arr2.length; x++){
+      if(arr1[i] == arr2[x]) duplicates.push(arr2[x]);
+    }
+  }
+
+  for(i=0; i<both.length; i++){
+    for(x=0; x<duplicates.length; x++){
+      var index = both.indexOf(duplicates[x]);
+      if(index != -1) both.splice(index, 1);
+    }
+  }
+  return both;
 }
